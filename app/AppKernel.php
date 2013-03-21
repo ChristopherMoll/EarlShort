0d846397e4e8cff0ca80d38056be893c6ba7d47b
chrismoll <senormoll@gmail.com>
2013-03-20 23:01:31 -0400

    Created FOS template overrides

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,8 @@ class AppKernel extends Kernel
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new EarlShort\MainBundle\EarlShortMainBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle()
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new FOS\UserBundle\FOSUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
