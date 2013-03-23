1006e4cacbcf123305adfd161fd90d5c186aff50
chrismoll <senormoll@gmail.com>
2013-03-22 21:50:43 -0400

    FOSUserbundle configuration

--- src/EarlShort/MainBundle/Entity/User.php
+++ src/EarlShort/MainBundle/Entity/User.php
@@ -3,6 +3,7 @@ namespace EarlShort\MainBundle\Entity;
 
 use FOS\UserBundle\Entity\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
@@ -26,33 +27,15 @@ class User extends BaseUser
      */
     protected $name;
 
-    /**
-     * @ORM\Column(type="string", length=18)
-     *
-     * @Assert\NotBlank(message="Please enter your username.", groups={"Registration", "Profile"})
-     * @Assert\MinLength(limit="3", message="The username is too short.", groups={"Registration", "Profile"})
-     * @Assert\MaxLength(limit="18", message="The username is too long.", groups={"Registration", "Profile"})
-     */
-
-    protected $username;
-
-    /**
-     * @Assert\Email(
-     *     message = "The email '{{ value }}' is not a valid email.",
-     *     checkMX = true
-     * )
-     */
-    protected $email;
 
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     *
-     * @Assert\NotBlank(message="Please enter your name.", groups={"Registration", "Profile"})
-     * @Assert\MinLength(limit="8", message="The name is too short.", groups={"Registration", "Profile"})
-     * @Assert\MaxLength(limit="64", message="The name is too long.", groups={"Registration", "Profile"})
-     */
-    protected $plainPassword;
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
 
 
 
